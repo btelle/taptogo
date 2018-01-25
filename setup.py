b46be88fa43696e93dba2214b53a32f4aed8e285
Brandon Telle <brandon.telle@gmail.com>
2018-01-24 21:23:24 -0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='taptogo',
     packages=['taptogo'],
-    version = "0.1",
+    version = "0.1.1",
     author = 'Brandon Telle',
     author_email = 'brandon.telle@gmail.com',
     url = 'https://github.com/btelle/taptogo',
-    download_url = 'https://github.com/btelle/taptogo/archive/0.1.tar.gz',
+    download_url = 'https://github.com/btelle/taptogo/archive/0.1.1.tar.gz',
     include_package_data=True,
     install_requires=[
         'selenium',
